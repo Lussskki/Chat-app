1be09c138366fe4120487154913bd43afa66dbfb
Luka Guledani <lukaguledani123@gmail.com>
2024-12-01 20:02:06 +0400

    clear from file socket.io, just have run the server

--- src/server.js
+++ src/server.js
@@ -1,37 +1,16 @@
 import express from 'express'
-import http from 'http'
-import { Server as SocketServer } from 'socket.io'
-import dotenv from 'dotenv'
-import path from 'path'
+import route from './routes/chatRoute.js'
 
-dotenv.config()
 
 const app = express()
-const server = http.createServer(app)
-const io = new SocketServer(server)
 
-const PORT = process.env.PORT || 3000
 
-app.use(express.json())
-app.use(express.static(path.resolve('')))
+app.use('/3000', route)
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./src/index.html'))
-})
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id)
 
-  socket.on('message', (data) => {
-    console.log('Message received:', data)
-    io.emit('message', data)
-  })
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id)
-  })
-})
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
 })
