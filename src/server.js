d04c2fd420ac30eb18a2d1ff2288451212e0351c
Luka Guledani <lukaguledani123@gmail.com>
2024-12-07 00:07:45 +0400

    add route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
-
 // Database
 import mDb from './database/mDb.js'
+// Routes
+import userRoute from './routes/userRoute.js'
 
 const server = express()
 const port = 5000
 
+server.use(express.json())
+
+server.use('/signup', userRoute)
 
 
 server.listen(port,() =>{
