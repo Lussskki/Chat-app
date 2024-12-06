bc2ff0df3cdfd9fa4757febbbdfb468c33fc532c
Lussskki <lukaguledani123@gmail.com>
2024-12-06 12:13:31 +0400

    implement database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
+// Database
+import mDb from './database/mDb.js'
+
 const server = express()
 const port = 5000
 
 
 
-server.listen(3000,() =>{
+server.listen(port,() =>{
     console.log(`Server listening to port ${port}`)
 })
