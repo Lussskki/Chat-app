8daa6a9e19562affe6f253e11ee29dcf019886c1
Luka Guledani <lukaguledani123@gmail.com>
2024-12-08 17:16:52 +0400

    add bcrypt to hash user password

--- src/controllers/signupController.js
+++ src/controllers/signupController.js
@@ -1,5 +1,6 @@
 import User from '../database/schemas/userSchema.js'
-
+import bcrypt from 'bcrypt'
+const saltRounds = 10
 
 const signup = async (req, res) => {
     const {username, password} = req.body
@@ -14,10 +15,21 @@ const signup = async (req, res) => {
         if(existingUser) {
             return res.status(400).json({message: 'Username is already taken'})
         }
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-        const newUser = new User({username, password})
+        // Create a new instance of UserSchema
+        const newUser = new User({ username, password: hashedPassword })
+
+        // Save to MongoDB 
         await newUser.save()
 
+        // Exclude password from the response
+        const userWithoutPassword = {
+        ...newUser.toObject(),
+        password: undefined, // Don't send the password back
+        }
+        
         return res.status(201).json({message: 'User added successfuly'})
     }catch(error){
         console.log(error)
